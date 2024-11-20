67b71c8ff07278b73a64cecd469dde7d28aeae9b
dtomilov <dtomilov@jaxel.com>
2024-11-20 22:02:30 +0000

    Start work on core soft for web app

--- project_root/core/views.py
+++ project_root/core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'base.html', {'title': 'Главная'})
+
+def about(request):
+    return render(request, 'about.html', {'title': 'О компании'})
+
+def contact(request):
+    return render(request, 'contact.html', {'title': 'Контакты'})
